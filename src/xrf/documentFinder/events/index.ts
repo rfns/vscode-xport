6fa039f7de3e5904cfe9ec56cb8e2f8836e6375b
Rubens F. N. da Silva <felipensgm@gmail.com>
2020-01-22 15:20:40 -0300

    scaffolding

--- src/xrf/documentFinder/events/index.ts
+++
@@ -1,9 +0,0 @@
-import * as onDidChangeValue from './onDidChangeValue'
-import * as onDidHide from './onDidHide'
-import * as onDidAccept from './onDidAccept'
-
-export {
-  onDidChangeValue,
-  onDidAccept,
-  onDidHide
-}
--- src/xrf/documentFinder/events/onDidHide.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from 'vscode'
-import { XRFDocumentQuickPickItem } from '../'
-
-export function register (quickPicker: vscode.QuickPick<XRFDocumentQuickPickItem>) {
-  quickPicker.onDidHide(() => quickPicker.dispose())
-}
